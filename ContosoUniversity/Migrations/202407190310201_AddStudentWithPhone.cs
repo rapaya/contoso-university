72aa5283a612205b5036481eb00fb225b11eb797
Daniel Rapaic <daniel.rapaic@gbi.co>
2024-07-18 23:34:48 -0400

    commit

---
+++ ContosoUniversity/Migrations/202407190310201_AddStudentWithPhone.cs
@@ -0,0 +1,18 @@
+﻿namespace ContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddStudentWithPhone : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Person", "PhoneNumber", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Person", "PhoneNumber", c => c.String(maxLength: 50));
+        }
+    }
+}
---
+++ ContosoUniversity/Models/StudentWithPhone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{   
+    public class StudentWithPhone : Student
+    {
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+    }
+}
